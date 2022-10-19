5e09c515c49e4519ee4c1b741a2ed39f5476ce73
ContellaJP <60985304+ContellaJP@users.noreply.github.com>
2022-10-18 22:27:21 -0500

    input validation

--- src/agridrone/view/DialogController.java
+++ src/agridrone/view/DialogController.java
@@ -21,259 +21,334 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class DialogController {
-	
-	@FXML
-    private Stage dialogBox;
-    
-	@FXML
-    private TabPane dialogContent;
-    
-	@FXML
-    private Tab renameTab;
 
 	@FXML
-    private Tab changeLocationTab;
-    
+	private Stage dialogBox;
+
 	@FXML
-    private Tab changePriceTab;
-    
+	private TabPane dialogContent;
+
 	@FXML
-    private Tab changeDimensionsTab;
-    
+	private Tab renameTab;
+
 	@FXML
-    private Tab addItemTab;
-    
+	private Tab changeLocationTab;
+
 	@FXML
-    private Tab addItemContTab;
-	
+	private Tab changePriceTab;
+
+	@FXML
+	private Tab changeDimensionsTab;
+
+	@FXML
+	private Tab addItemTab;
+
+	@FXML
+	private Tab addItemContTab;
+
 	@FXML
 	private Button dialogCancelButton;
-	
+
 	@FXML
 	private Button applyButton;
-	
+
 	@FXML
 	private TextField renameBox;
-	
+
 	@FXML
 	private TextField xCoordBox;
-	
+
 	@FXML
 	private TextField yCoordBox;
-	
+
 	@FXML
 	private TextField priceBox;
-	
+
 	@FXML
 	private TextField widthBox;
-	
+
 	@FXML
 	private TextField lengthBox;
-	
+
 	@FXML
 	private TextField newItemName;
-	
+
 	@FXML
 	private TextField newItemX;
-	
+
 	@FXML
 	private TextField newItemY;
-	
+
 	@FXML
 	private TextField newItemPrice;
-	
+
 	@FXML
 	private TextField newItemWidth;
-	
+
 	@FXML
 	private TextField newItemLength;
-	
+
 	@FXML
 	private TextField newContainerName;
-	
+
 	@FXML
 	private TextField newContainerX;
-	
+
 	@FXML
 	private TextField newContainerY;
-	
+
 	@FXML
 	private TextField newContainerPrice;
-	
+
 	@FXML
 	private TextField newContainerWidth;
-	
+
 	@FXML
 	private TextField newContainerLength;
+
+	@FXML
+	private Label inputErrorLocation;
+
+	@FXML
+	private Label inputErrorPrice;
+	
+	@FXML
+	private Label inputErrorDimensions;
+
+	@FXML
+	private Label inputErrorAddItem;
+	
+	@FXML
+	private Label inputErrorAddItemContainer;
 	
 
-    
-	
+
 	private static DialogController dialogController;
-    
-	
-    public DialogController() throws LoadException {
-        if (dialogController == null) dialogController = this;
-        else throw new LoadException("Singleton FXML");
-    }
-    
-    public void setStage(Stage dialogBox) {
-        this.dialogBox = dialogBox;
-    }
 
-    public void removeController() {
-        dialogController = null;
-    }
+	public DialogController() throws LoadException {
+		if (dialogController == null)
+			dialogController = this;
+		else
+			throw new LoadException("Singleton FXML");
+	}
 
-    private void closeStage() {
-        dialogBox.close();
-        removeController();
-    }
-   
-    @FXML
-    public void cancelDialog() {
-    	closeStage();
-    }
-    
-    public void buildDialogBox(int c, TreeItem<ItemAbstract> item) {
-    	SingleSelectionModel<Tab> selectionModel = dialogContent.getSelectionModel();
-    	dialogBox.setTitle(item.getValue().getName() + " Actions");
-    	
-    	if (item.getValue() instanceof Item) {
-    		addItemTab.setDisable(true);
-    		addItemContTab.setDisable(true);
-    	} else {
-    		addItemTab.setDisable(false);
-    		addItemContTab.setDisable(false);
-    	}
-    	
-    	//switch case for building dialog box
-    	switch (c) {
-    		//rename
-    		case 0:
-    			selectionModel.select(c);
-    			
-    			applyButton.setOnAction(new EventHandler<ActionEvent>() {
+	public void setStage(Stage dialogBox) {
+		this.dialogBox = dialogBox;
+	}
 
-					@Override
-					public void handle(ActionEvent e) {
-						String newName = renameBox.getText();
-						item.getValue().setName(newName);
-						cancelDialog();
+	public void removeController() {
+		dialogController = null;
+	}
+
+	private void closeStage() {
+		dialogBox.close();
+		removeController();
+	}
+
+	@FXML
+	public void cancelDialog() {
+		closeStage();
+	}
+
+	public void buildDialogBox(int c, TreeItem<ItemAbstract> item) {
+		SingleSelectionModel<Tab> selectionModel = dialogContent.getSelectionModel();
+		dialogBox.setTitle(item.getValue().getName() + " Actions");
+
+		if (item.getValue() instanceof Item) {
+			addItemTab.setDisable(true);
+			addItemContTab.setDisable(true);
+		} else {
+			addItemTab.setDisable(false);
+			addItemContTab.setDisable(false);
+		}
+
+		// switch case for building dialog box
+		switch (c) {
+		// rename
+		case 0:
+			selectionModel.select(c);
+
+			applyButton.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent e) {
+					String newName = renameBox.getText();
+					item.getValue().setName(newName);
+					cancelDialog();
+				}
+
+			});
+			break;
+		// change location
+		case 1:
+			selectionModel.select(c);
+
+			applyButton.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent e) {
+
+					// input error catching
+					if (xCoordBox.getText() == null || yCoordBox.getText() == null) {
+						inputErrorLocation.setVisible(true);
 					}
-    				
-    			});
-    			break;
-    		//change location
-    		case 1:
-    			selectionModel.select(c);
-    			
-    			applyButton.setOnAction(new EventHandler<ActionEvent>() {
+					try {
+						int xTest = Integer.parseInt(xCoordBox.getText());
+						int yTest = Integer.parseInt(yCoordBox.getText());
 
-					@Override
-					public void handle(ActionEvent e) {
-						
-						
-						int newX = Integer.parseInt(xCoordBox.getText());
-						item.getValue().setLocationX(newX);
-						int newY = Integer.parseInt(yCoordBox.getText());
-						item.getValue().setLocationY(newY);
-						cancelDialog();
+					} catch (NumberFormatException nfe) {
+						inputErrorLocation.setVisible(true);
 					}
-    				
-    			});
-    			
-    			
-    			break;
-    		//change price
-    		case 2: 
-    			selectionModel.select(c);
-    			
-    			applyButton.setOnAction(new EventHandler<ActionEvent>() {
 
-					@Override
-					public void handle(ActionEvent e) {
-						Float newPrice = Float.parseFloat(priceBox.getText());
-						item.getValue().setPrice(newPrice);
-						cancelDialog();
-						
+					int newX = Integer.parseInt(xCoordBox.getText());
+					item.getValue().setLocationX(newX);
+					int newY = Integer.parseInt(yCoordBox.getText());
+					item.getValue().setLocationY(newY);
+					cancelDialog();
+				}
+
+			});
+
+			break;
+		// change price
+		case 2:
+			selectionModel.select(c);
+
+			applyButton.setOnAction(new EventHandler<ActionEvent>() {
+
+
+				@Override
+				public void handle(ActionEvent e) {
+
+					if (priceBox.getText() == null) {
+						inputErrorPrice.setVisible(true);
+					}
+					try {
+						Float tryPrice = Float.parseFloat(priceBox.getText());
+
+					} catch (NumberFormatException nfe) {
+						inputErrorPrice.setVisible(true);
+					}
+					Float newPrice = Float.parseFloat(priceBox.getText());
+					item.getValue().setPrice(newPrice);
+					cancelDialog();
+
+				}
+
+			});
+
+			break;
+		// change dimensions
+		case 3:
+			selectionModel.select(c);
+			applyButton.setOnAction(new EventHandler<ActionEvent>() {
 				
+				@Override
+				public void handle(ActionEvent e) {
+					
+					if (lengthBox.getText() == null || widthBox.getText() == null) {
+						inputErrorDimensions.setVisible(true);
 					}
-    				
-    			});
-    			
-    			break;
-    		//change dimensions
-    		case 3:
-    			selectionModel.select(c);
-    			applyButton.setOnAction(new EventHandler<ActionEvent>() {
-
-					@Override
-					public void handle(ActionEvent e) {
+					try {
+						int testL = Integer.parseInt(lengthBox.getText());
+						int testW = Integer.parseInt(widthBox.getText());
 						
-						int newLength = Integer.parseInt(lengthBox.getText());
-						item.getValue().setLength(newLength);
-						int newWidth = Integer.parseInt(widthBox.getText());
-						item.getValue().setWidth(newWidth);
-						cancelDialog();
-				
+					} catch(NumberFormatException nfe) {
+						inputErrorDimensions.setVisible(true);
 					}
-    				
-    			});
-    			
-    			break;
-    		//add item
-    		case 4:
-    			selectionModel.select(c);
-    			
-    			applyButton.setOnAction(new EventHandler<ActionEvent>() {
 
-					@Override
-					public void handle(ActionEvent e) {
-						
-						int x = Integer.parseInt(newItemX.getText());
+					int newLength = Integer.parseInt(lengthBox.getText());
+					item.getValue().setLength(newLength);
+					int newWidth = Integer.parseInt(widthBox.getText());
+					item.getValue().setWidth(newWidth);
+					cancelDialog();
+
+				}
+
+			});
+
+			break;
+		// add item
+		case 4:
+			selectionModel.select(c);
+
+			applyButton.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent e) {
+					
+
+					
+					if (newItemPrice.getText() == null || newItemY.getText() == null || newItemWidth.getText() == null || newItemLength.getText() == null || newItemPrice.getText() == null) {
+						inputErrorAddItem.setVisible(true);
+					}
+					
+					try {
+						int x = Integer.parseInt(newItemPrice.getText());
 						int y = Integer.parseInt(newItemY.getText());
 						int w = Integer.parseInt(newItemWidth.getText());
 						int l = Integer.parseInt(newItemLength.getText());
 						int p = Integer.parseInt(newItemPrice.getText());
-						
-						Item newItem = new Item(newItemName.getText(), x, y, w, l, p);
-						item.getValue().addItemAbstract(newItem);
-						cancelDialog();
-				
+					} catch (NumberFormatException nfe) {
+						inputErrorAddItem.setVisible(true);
 					}
-    				
-    			});
-    			
-    			break;
-    		//add item container
-    		case 5:
-    			selectionModel.select(c);
-    			applyButton.setOnAction(new EventHandler<ActionEvent>() {
+					
+					int x = Integer.parseInt(newItemX.getText());
+					int y = Integer.parseInt(newItemY.getText());
+					int w = Integer.parseInt(newItemWidth.getText());
+					int l = Integer.parseInt(newItemLength.getText());
+					int p = Integer.parseInt(newItemPrice.getText());
 
-					@Override
-					public void handle(ActionEvent e) {
-						
-						int x = Integer.parseInt(newContainerX.getText());
-						int y = Integer.parseInt(newContainerY.getText());
-						int w = Integer.parseInt(newContainerWidth.getText());
-						int l = Integer.parseInt(newContainerLength.getText());
-						int p = Integer.parseInt(newContainerPrice.getText());
-						
-						ItemContainer newContainer = new ItemContainer(newContainerName.getText(), x, y, w, l, p);
-						item.getValue().addItemContainer(newContainer); //doesnt work for now it looks like it just makes an item 10-17 -JP
-						cancelDialog();
-				
+					Item newItem = new Item(newItemName.getText(), x, y, w, l, p);
+					item.getValue().addItemAbstract(newItem);
+					cancelDialog();
+
+				}
+
+			});
+
+			break;
+		// add item container
+		case 5:
+			selectionModel.select(c);
+			applyButton.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent e) {
+					
+					if (newContainerX.getText() == null || newContainerY.getText() == null || newContainerWidth.getText() == null || newContainerLength.getText() == null || newContainerPrice.getText() == null) {
+						inputErrorAddItemContainer.setVisible(true);
 					}
-    				
-    			});
-    			
-    			
-    			
-    			break;
-    					
-    	}
-    	
-    	
-    }
+					
+				try {
+					int x = Integer.parseInt(newContainerX.getText());
+					int y = Integer.parseInt(newContainerY.getText());
+					int w = Integer.parseInt(newContainerWidth.getText());
+					int l = Integer.parseInt(newContainerLength.getText());
+					int p = Integer.parseInt(newContainerPrice.getText());
+					
+				}catch(NumberFormatException nfe) {
+					inputErrorAddItemContainer.setVisible(true);
+				}
+
+					int x = Integer.parseInt(newContainerX.getText());
+					int y = Integer.parseInt(newContainerY.getText());
+					int w = Integer.parseInt(newContainerWidth.getText());
+					int l = Integer.parseInt(newContainerLength.getText());
+					int p = Integer.parseInt(newContainerPrice.getText());
+
+					ItemContainer newContainer = new ItemContainer(newContainerName.getText(), x, y, w, l, p);
+					item.getValue().addItemAbstract(newContainer); 
+																
+					cancelDialog();
+
+				}
+
+			});
+
+			break;
+
+		}
+
+	}
 }
